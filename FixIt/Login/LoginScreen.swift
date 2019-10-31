d2691b3c500a7ed286c051155e7c7a4159d773aa
Josiah <Agosto_Josiah@yahoo.com>
2019-10-31 19:07:32 -0400

    Added Firebase.

---
+++ FixIt/Login/LoginScreen.swift
@@ -0,0 +1,19 @@
+//
+//  LoginScreen.swift
+//  FixIt
+//
+//  Created by Josiah Agosto on 10/31/19.
+//  Copyright © 2019 Josiah Agosto. All rights reserved.
+//
+
+import UIKit
+
+class LoginScreen: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
